ec39222819fdd24ed9ee4e71b4e314573035a6a2
volodpj <volod.af@gmail.com>
2019-08-05 17:55:28 +0300

    slider

--- index.js
+++ index.js
@@ -4,35 +4,41 @@
 let sliderLeft = document.getElementById('leftPlay');
 let sliderRight = document.getElementById('rightPlay');
 
-let left = 0;
-sliderLeft.addEventListener('click', () => {
-  let line = document.querySelector('.line');
-  left = left - 725;
-  if(left < -1450){
-    left = 0;
-  }
-  line.style.left = left + 'px';
-});
+let slideIndex = 1;
 
-sliderRight.addEventListener('click', () => {
-  let line = document.querySelector('.line');
-  left = left + 725;
-  if(left > 0){
-    left = -1450;
+const plusSliders = (n) =>{
+  showSliders(slideIndex += n);
+};
+const currentSliders = (n) =>{
+  showSliders(slideIndex = n);
+};
+
+const showSliders = (n) =>{
+  let i;
+  let sliders = document.querySelectorAll('.slider__item');
+  let dots = document.querySelectorAll('.dots__dot');
+  if( n > sliders.length ){
+    slideIndex = 1;
   }
- 
-  line.style.left = left + 'px';
-})
+  if( n < 1){
+    slideIndex = sliders.length;
+  }
+  for(i = 0; i < sliders.length; i++){
+    sliders[i].style.display = 'none'
+  }
+  for(i = 0; i < sliders.length; i++){
+    dots[i].className = dots[i].className.replace("active","");
+  }
+  sliders[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
 
 setInterval(() => {
-    let line = document.querySelector('.line');
-    left = left - 725;
-    if(left < -1450){
-      left = 0;
-    }
-    line.style.left = left + 'px';
-  },
-5000);
+  plusSliders(1)
+}, 5000)
+showSliders(slideIndex);
+
+
 
 const photoshop = document.querySelector('.photoshop');
 const html = document.querySelector('.html');
